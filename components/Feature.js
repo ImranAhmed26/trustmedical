3d9b2a1afd717f88fae0fdbbd41fdcd63030f977
Imran Ahmed <imran.ahk26@gmail.com>
2024-03-04 02:43:05 +0600

    remove

--- components/Feature.js
+++ components/Feature.js
@@ -24,7 +24,7 @@ const Feature = () => {
           <motion.div className="h-full w-full p-4" variants={scrollAnimation}>
             <Image
               src="/assets/banner2.jpg"
-              alt="VPN Illustrasi"
+              alt=" Illustrasi"
               layout="responsive"
               quality={100}
               height={350}
